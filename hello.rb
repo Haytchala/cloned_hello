5212c31da4500ca1702c9c9ab839e992da3c8c11
haytchala <haytchala@outlook.com>
2015-09-08 20:58:19 -0300

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,7 +1,5 @@
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{haytchala}!"
-# Default is "World"
-name = ARGV.first || "World"
-
-puts "Hello, #{haytchala}!"
+puts "Hello, #{name}!"
